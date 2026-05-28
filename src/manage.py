b62eb5ca062f8be20804402e04f9f19a94dd6f6c
TheDocingEast <116012307+TheDocingEast@users.noreply.github.com>
2026-05-28 23:16:55 +0300

    Recreated for NaviTime kanban

--- src/manage.py
+++ src/manage.py
@@ -3,11 +3,11 @@
 
 import os
 import sys
-from django.core.management import execute_from_command_line
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library_api.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "navitime_kanban.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
